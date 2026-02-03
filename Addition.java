73bcb21a92f1fa041ce6f3b7a3b03b44c49ae8b2
tekusr0000 <tekusr0000@teknoturf.net>
2026-02-03 17:05:43 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("First number : " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Sum is       : " + sum);
+    }
+}
